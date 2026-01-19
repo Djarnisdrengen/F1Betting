50ac325645deb4547976662397ee1ece65be6634
Djarnisdrengen <122156356+Djarnisdrengen@users.noreply.github.com>
2026-01-19 14:43:31 +0100

    Delete all unused emergent files for the react application

--- php_version/public/migration_betting_window.sql
+++
@@ -1,7 +0,0 @@
--- Migration: Add betting window hours setting
--- Run this in phpMyAdmin if you already have the database set up
-
-ALTER TABLE settings ADD COLUMN betting_window_hours INT DEFAULT 48;
-
--- Update existing row
-UPDATE settings SET betting_window_hours = 48 WHERE id = 1;
