f29697ea07a852988b1902ec576fca319f68f4eb
Djarnisdrengen <122156356+Djarnisdrengen@users.noreply.github.com>
2026-01-22 20:50:42 +0100

    testin local folder sync

--- testsuite/tests/bootstrap.php
+++ testsuite/tests/bootstrap.php
@@ -5,5 +5,5 @@ require_once __DIR__ . '/../vendor/autoload.php';
 // Manually require the base classes that aren't in Composer
 require_once __DIR__ . '/BaseTestCase.php';
 require_once __DIR__ . '/AuthenticatedClient.php';
-
+//xxx
 echo "DEBUG: Bootstrap loaded successfully.\n";
