943f0580793e8e2fbe0d6012172275659fdf7a54
Djarnisdrengen <thomas@helvegpovlsen.dk>
2026-01-22 22:27:35 +0100

    config.php

--- testsuite/tests/GatedAreaTest.php
+++ testsuite/tests/GatedAreaTest.php
@@ -4,14 +4,38 @@ use PHPUnit\Framework\TestCase;
 
 class GatedAreaTest extends TestCase
 {
+    private $client;
+
+    protected function setUp(): void{
+        $this->client = $GLOBALS['authClient'];    
+    }
+
     public function testAuthenticatedAccess()
-    {
-        $client = $GLOBALS['authClient'];
-        list($status, $body) = $client->get('index.php');
+    {       
+        list($status, $body) = $this->client->get('index.php');
 
         $this->assertEquals(200, $status);
-        $this->assertStringContainsString('Log ud', $body);
-        $this->assertStringContainsString('Placer Bet', $body);
+        $this->assertStringContainsString('Log ud', $body);      
 
     }
+
+    public function testPlaceBet()
+    {       
+       
+        list($status, $body) = $this->client->get('index.php');
+
+        //Validate bet button is available
+        $this->assertEquals(200, $status);
+        $this->assertStringContainsString('Placer Bet', $body); 
+        
+        //Validate bet page is accessible
+        list($status, $body) = $this->client->get('bet.php');
+        $this->assertEquals(200, $status);
+        $this->assertStringContainsString('Placer Bet', $body); 
+        $this->assertStringContainsString('Annuller', $body); 
+
+
+
+    }
+
 }
