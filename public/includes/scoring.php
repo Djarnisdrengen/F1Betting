59e015a289c9c8f7c2b97c985b6c261915ec5012
Thomas Helveg Povlsen <thomas@helvegpovlsen.dk>
2026-05-07 21:18:05 +0200

    fix: use getDB() instead of global $db in scoring.php

--- public/includes/scoring.php
+++ public/includes/scoring.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . '/../../config.php';
 
 function calculateRacePoints($raceId, $p1, $p2, $p3) {
-    global $db;
+    $db = getDB();
     $results = [$p1, $p2, $p3];
 
     $settings       = getSettings();
